ddb42485f5025c3ada0b959771efb704bddd4deb
William M <wmooney1984@gmail.com>
2019-04-27 20:50:28 -0400

    Loan Calculator: Loader & UX

--- ModernJavaScript/Section 04/loan-calculator/app.js
+++ ModernJavaScript/Section 04/loan-calculator/app.js
@@ -1,8 +1,18 @@
 // Listen for submit
-document.getElementById("loan-form").addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function(e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show Loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e) {
+function calculateResults() {
   console.log("Calculating...");
   // UI Vars
   const amount = document.getElementById('amount');
@@ -24,14 +34,17 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('results').style.display = 'block';
   } else {
     showError('Please check your numbers');
   }
-
-  e.preventDefault();
 }
 
 function showError(error) {
+  document.getElementById('loading').style.display = 'none';
+  document.getElementById('results').style.display = 'none';
   const errorDiv = document.createElement('div');
   const card = document.querySelector('.card');
   const heading = document.querySelector('.heading');
